fix: add Batch-Normalization layer to Actor & Critic Networks
To speed up the training, implemented a Batch-Normalization layer
behind the first layer of Actor Network and Critic Network.

ddpg_agent.py
@@ -10,13 +10,13 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 BUFFER_SIZE = int(1e6)  # replay buffer size
-BATCH_SIZE = 64         # minibatch size
+BATCH_SIZE = 128         # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
 LR_ACTOR = 1e-4         # learning rate of the actor 
-LR_CRITIC = 3e-4        # learning rate of the critic
+LR_CRITIC = 1e-4        # learning rate of the critic
 WEIGHT_DECAY = 0.0001   # L2 weight decay
-UPDATE_EVERY = 5        # how often to update the network ###
+UPDATE_EVERY = 10        # how often to update the network ###
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -65,8 +65,9 @@ class Agent():
         self.t_step = (self.t_step + 1) % UPDATE_EVERY
         if self.t_step == 0:
             if len(self.memory) > BATCH_SIZE:
-                experiences = self.memory.sample()
-                self.learn(experiences, GAMMA)
+                for _ in range(UPDATE_EVERY):
+                    experiences = self.memory.sample()
+                    self.learn(experiences, GAMMA)
 #         if len(self.memory) > BATCH_SIZE:
 #             experiences = self.memory.sample()
 #             self.learn(experiences, GAMMA)